2c3759335e1230d31ab2aed8bce264eb932bae0b
Attack Cristina <klimenko.an11@gmail.com>
2022-11-01 16:57:00 +0300

    reverse_iterator: done, commit.sh: added

--- inc/random_access_iterator.h
+++ inc/random_access_iterator.h
@@ -8,7 +8,133 @@
 #include <iostream>
 
 namespace ft {
+    // https://cplusplus.com/reference/iterator/iterator/
+    template <class Category, class T, class Distance = ptrdiff_t,
+            class Pointer = T*, class Reference = T&>
+    struct iterator {
+        typedef T         value_type;
+        typedef Distance  difference_type;
+        typedef Pointer   pointer;
+        typedef Reference reference;
+        typedef Category  iterator_category;
+    };
 
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag {};
+    struct bidirectional_iterator_tag {};
+    struct random_access_iterator_tag {};
+
+    // https://cplusplus.com/reference/iterator/iterator_traits/
+    template <class Iterator>
+    class iterator_traits {
+    public:
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
+
+    // https://cplusplus.com/reference/iterator/iterator_traits/
+    template <class T>
+    class iterator_traits<T*> {
+    public:
+        typedef typename ptrdiff_t                      difference_type;
+        typedef typename T                              value_type;
+        typedef typename T*                             pointer;
+        typedef typename T&                             reference;
+        typedef typename ft::random_access_iterator_tag iterator_category;
+    };
+
+    // https://cplusplus.com/reference/iterator/iterator_traits/
+    template <class T>
+    class iterator_traits<const T*> {
+    public:
+        typedef typename ptrdiff_t                      difference_type;
+        typedef typename T                              value_type;
+        typedef typename const T*                       pointer;
+        typedef typename const T&                       reference;
+        typedef typename ft::random_access_iterator_tag iterator_category;
+    };
+
+    // https://cplusplus.com/reference/iterator/reverse_iterator/
+    template <class Iterator>
+    class reverse_iterator {
+    public:
+        typedef          Iterator                                           iterator_type;
+        typedef typename ft::iterator_traits<Iterator>::iterator_category   iterator_category;
+        typedef typename ft::iterator_traits<Iterator>::value_type          value_type;
+        typedef typename ft::iterator_traits<Iterator>::difference_type     difference_type;
+        typedef typename ft::iterator_traits<Iterator>::pointer             pointer;
+        typedef typename ft::iterator_traits<Iterator>::reference           reference;
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/reverse_iterator/
+        reverse_iterator() : _it() {};
+        explicit reverse_iterator (iterator_type it) : _it(it) {};
+        template <class Iter>
+        reverse_iterator (const reverse_iterator<Iter>& rev_it) : _it(rev_it.base()) {};
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/base/
+        iterator_type base() const { return _it; };
+
+        reverse_iterator operator= (reverse_iterator src) {
+            _it = src._it;
+            return (*this);
+        }
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator*/
+        reference operator*() const { return *_it; }; // correct ?
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator+/
+        reverse_iterator operator+ (difference_type n) const { return reverse_iterator(_it - n); };
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator++/
+        reverse_iterator& operator++() {
+            _it--;
+            return (*this);
+        };
+        reverse_iterator operator++(int) {
+            reverse_iterator temp = *this;
+            ++(*this);
+            return temp;
+        };
+
+        //https://cplusplus.com/reference/iterator/reverse_iterator/operator+=/
+        reverse_iterator& operator+= (difference_type n) {
+            (*this) = (*this) + n;
+            return (*this);
+        };
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator-/
+        reverse_iterator operator- (difference_type n) const { return reverse_iterator(_it + n); };
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator--/
+        reverse_iterator& operator--() {
+            _it++;
+            return (*this);
+        };
+        reverse_iterator operator--(int) {
+            reverse_iterator temp = *this;
+            --(*this);
+            return temp;
+        };
+
+        //https://cplusplus.com/reference/iterator/reverse_iterator/operator-=/
+        reverse_iterator& operator-= (difference_type n) {
+            (*this) = (*this) - n;
+            return (*this);
+        };
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator-%3E/
+        pointer operator->() const { return &(operator*()); };
+
+        // https://cplusplus.com/reference/iterator/reverse_iterator/operator[]/
+        reference operator[] (difference_type n) const { return base()[-n - 1]; };
+
+    private:
+        iterator_type _it;
+    };
 }
 
 #endif //RANDOM_ACCESS_ITERATOR_H
